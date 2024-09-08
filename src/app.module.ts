ad45caf74675de047282c43dbda89733afe44288
Tanvir Hossain <tanvirhossain.butex@gmail.com>
2024-09-08 16:47:27 +0600

    implemented error formatter globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ResponseTransformInterceptor } from './common/interceptors/response-transform.interceptor';
 
 @Module({
@@ -9,6 +10,10 @@ import { ResponseTransformInterceptor } from './common/interceptors/response-tra
         {
             provide: APP_INTERCEPTOR,
             useClass: ResponseTransformInterceptor,
+        },
+        {
+            provide: APP_FILTER,
+            useClass: HttpExceptionFilter,
         },
     ],
 })
