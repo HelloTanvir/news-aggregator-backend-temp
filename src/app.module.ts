1660ec9a8b5344f8713e1a005fb4cb2500f589bd
Tanvir Hossain <tanvirhossain.butex@gmail.com>
2024-09-08 16:45:33 +0600

    implemented global response formatter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { ResponseTransformInterceptor } from './common/interceptors/response-transform.interceptor';
 
 @Module({
     imports: [ConfigModule.forRoot({ isGlobal: true })],
-    providers: [],
+    providers: [
+        {
+            provide: APP_INTERCEPTOR,
+            useClass: ResponseTransformInterceptor,
+        },
+    ],
 })
 export class AppModule {}
