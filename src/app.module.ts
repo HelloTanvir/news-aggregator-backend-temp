d508b81f3add1ef25eca4eadf893ea20e0189bb1
Tanvir Hossain <tanvirhossain.butex@gmail.com>
2024-09-08 16:54:23 +0600

    implemented database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ResponseTransformInterceptor } from './common/interceptors/response-transform.interceptor';
+import { DBModule } from './db/db.module';
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true })],
+    imports: [ConfigModule.forRoot({ isGlobal: true }), DBModule],
     providers: [
         {
             provide: APP_INTERCEPTOR,
