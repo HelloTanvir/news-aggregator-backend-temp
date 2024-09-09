cfb97f6f3093ee3f02f2d4b161817daa83bf0cf9
Tanvir Hossain <tanvirhossain.butex@gmail.com>
2024-09-09 12:25:15 +0600

    implemented JWT access and refresh token

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { AtGuard } from './common/guards/access-token.guard';
 import { ResponseTransformInterceptor } from './common/interceptors/response-transform.interceptor';
 import { DBModule } from './db/db.module';
 
 @Module({
     imports: [ConfigModule.forRoot({ isGlobal: true }), DBModule],
     providers: [
+        {
+            provide: APP_GUARD,
+            useClass: AtGuard,
+        },
         {
             provide: APP_INTERCEPTOR,
             useClass: ResponseTransformInterceptor,
