c89576264618326bb0c21fa4b1dc60792cd0f5c4
Tanvir Hossain <tanvirhossain.butex@gmail.com>
2024-09-10 15:14:55 +0600

    imported missing modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from '../user/user.module';
+import { UtilsModule } from '../utils/utils.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { Auth } from './entities/auth.entity';
 
 @Module({
-    imports: [JwtModule.register({}), TypeOrmModule.forFeature([Auth]), UserModule],
+    imports: [JwtModule.register({}), TypeOrmModule.forFeature([Auth]), UserModule, UtilsModule],
     controllers: [AuthController],
     providers: [AuthService],
 })
